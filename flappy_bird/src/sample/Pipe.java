d696a0581d1fb8ce71d29cd748eedad8d727bd27
Louis Bovay <louis.bovay@divtec.ch>
2020-11-02 14:06:40 +0100

    Update Pipe.java

--- flappy_bird/src/sample/Pipe.java
+++ flappy_bird/src/sample/Pipe.java
@@ -7,14 +7,15 @@ import java.awt.*;
 /**
  * Classe qui représente un tuyau
  */
-public class Pipe extends Sprite{
+public class Pipe extends Sprite {
 
     /**
      * Crée et instantie un tuyau
-     * @param x coordonée X
-     * @param y coordonnée Y
-     * @param w Largeur
-     * @param h Hauteur
+     *
+     * @param x     coordonée X
+     * @param y     coordonnée Y
+     * @param w     Largeur
+     * @param h     Hauteur
      * @param color Couleur
      */
     Pipe(int x, int y, int w, int h, Color color) {
@@ -22,9 +23,22 @@ public class Pipe extends Sprite{
 
     }
 
-
-
-
+    /**
+     * Check si le tuyeau se fait toucher par un oiseau
+     *
+     * @param oiseau qu'on surveille
+     * @return true = l'oiseau a touché / false l'oiseau n'a pas touché
+     */
+    public boolean isHit(Oiseau oiseau) {
+        boolean isTouched = false;
+        if (oiseau.getArea().getTopRight().getX() < this.getArea().getDownRight().getX()
+                && oiseau.getArea().getTopRight().getX() > this.getArea().getDownLeft().getX()
+                && oiseau.getArea().getTopRight().getY() < this.getArea().getDownLeft().getY()
+                && oiseau.getArea().getTopRight().getY() > this.getArea().getTopLeft().getY()) {
+            isTouched = true;
+        }
+        return isTouched;
+    }
 
 
 }
