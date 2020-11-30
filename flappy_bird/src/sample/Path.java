17298c774e7a1aa2eadf343620265c7325bcac5f
Louis Bovay <louis.bovay@divtec.ch>
2020-11-30 14:41:08 +0100

    Classe static qui offre des chemin de dossiers

---
+++ flappy_bird/src/sample/Path.java
@@ -0,0 +1,9 @@
+package sample;
+
+/**
+ * Chemin d'accès vers différents dossiers du projet
+ */
+public class Path {
+    public final static String DIR_SPRITES = "Sprites/";
+    public final static String DIR_FONT = "police/";
+}
