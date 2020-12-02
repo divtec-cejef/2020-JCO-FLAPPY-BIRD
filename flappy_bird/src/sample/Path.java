1ffdea819925e0260d34e48693f437f590844c3b
Louis Bovay <louis.bovay@divtec.ch>
2020-12-02 11:28:57 +0100

    Delete Path.java

--- flappy_bird/src/sample/Path.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-/**
- * Chemin d'accès vers différents dossiers du projet
- */
-public class Path {
-    public final static String DIR_SPRITES = "Sprites/";
-    public final static String DIR_FONT = "police/";
-}
